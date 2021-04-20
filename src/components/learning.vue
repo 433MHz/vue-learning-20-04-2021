2b12dfa836f8bd25f46f8c799995d14ad7c41e2b
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-21 01:55:07 +0200

    Class binding example

--- src/components/learning.vue
+++ src/components/learning.vue
@@ -1,20 +1,12 @@
 <template>
     <div>
-        <div v-for="book in books" v-bind:key="book.title">
-            <img :src='book.img' :alt="book.title">
-            <h2 v-show="showBooks">{{book.title}} - {{book.author}}</h2><br>
+        <div v-for="book in books" :key="book.title">
+            <div class="bookDiv" v-if="showBooks" :class="{bookDivFav: book.isFav}" >
+                <h3>{{book.title}}</h3> <h5> writen by: {{book.author}}</h5>
+            </div>
         </div>
        
         <button v-on:click="toogleBooks"><span v-if="!showBooks">Show books</span> <span v-else>Hide books</span></button><br>
-        <button @click="toggleLink">
-            <span v-if="link3 === link2">Youtube</span> 
-            <span v-else-if="link3 === link1">Google</span>
-            <span v-else>Unknown site</span>
-        </button><br>
-
-        <a :href = link3>link</a>
-        
-        <div id="bottomDiv">Icons made by <a href="https://www.flaticon.com/authors/flat-icons" title="Flat Icons">Flat Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </div>
 </template>
 
@@ -28,9 +20,9 @@ export default {
             showBooks: true,
 
             books:[
-                {title: 'Hyperion', author: 'Daniel Simon', img: "../src/assets/1.png"},
-                {title: 'Robinson Cruzoe', author:'Daniel Defoe', img: "../assets/2.png"},
-                {title: 'Vue', author:'Vue maker', img: "../assets/3.png"}
+                {title: 'Hyperion', author: 'Daniel Simon', isFav: true},
+                {title: 'Robinson Cruzoe', author:'Daniel Defoe', isFav: false},
+                {title: 'Vue', author:'Vue maker', isFav: true}
             ],
 
             link1: 'https://www.google.pl/',
@@ -72,5 +64,23 @@ export default {
 
     #bottomDiv{
         margin-top: 400px;
+    }
+
+    .bookDiv{
+        background-color: lightgray;
+        width: 500px;
+        display: inline-block;
+        height: 100px;
+        margin: 10px;
+        border-radius: 10px;
+    }
+
+    .bookDivFav{
+        background-color: lightgreen;
+        width: 500px;
+        display: inline-block;
+        height: 100px;
+        margin: 10px;
+        border-radius: 10px;
     }
 </style>
