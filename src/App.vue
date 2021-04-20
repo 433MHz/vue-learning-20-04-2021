09480310e478331b5ed315ad68098fbb8a72493e
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-20 21:55:48 +0200

    Buttons increasing age value

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <learning></learning>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+<script>
+import learning from './components/learning.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { learning },
+  
 }
-</script>
+</script>Learning
+
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body{
+    text-align: center;
+  }
 </style>
---
+++ src/components/learning.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>{{title}} - {{author}} - {{age}}</h2><br>
+
+        <button v-on:click="age = age + 1"> Dodaj rok </button><br>
+        <button v-on:click="removeOneYear()">Odejmij rok</button>
+    </div>
+</template>
+
+
+
+
+<script>
+export default {
+    data(){
+        return{
+            title: 'tytuł książki',
+            author: 'autor książki',
+            age: 45
+        }
+    },
+
+    methods:{
+        removeOneYear(){
+            this.age = this.age - 1;
+        }
+    }
+}
+</script>
