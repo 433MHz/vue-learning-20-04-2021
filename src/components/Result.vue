b728435517f3ab50f9492a5fd16bca16600d144b
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-24 06:42:33 +0200

    Added rank update depending on timer value

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,11 +1,27 @@
 <template>
-    {{timer}}
+    {{timer}}<br>
+    {{rank}}
 </template>
 
 
 <script>
 export default {
-    props:['timer']
+    props:['timer'],
+
+    data(){
+        return{
+            rank: null,
+            slow: 'Slow rank',
+            medium: 'Normal rank',
+            fast: 'Fast rank'
+        }
+    },
+
+    mounted(){
+        if(this.timer < 390){this.rank = this.fast}
+        else if(this.timer > 390 && this.timer < 500){this.rank = this.medium}
+        else{this.rank = this.slow}
+    }
 }
 </script>
 
