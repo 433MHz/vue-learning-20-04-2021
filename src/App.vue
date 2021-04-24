c687de0e438da4a9a57c477f8aea66faed0042be
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-24 08:53:09 +0200

    Example of v-model two way data binding

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,18 @@
 <template>
 <div id="container">
-    <h1>Reaction timer</h1>    
-    <button @click="start" :disabled="isPlaying">play</button><br>
-    <block :delay="delay" v-if="isPlaying" @reactionTime="emitedFromBlock($event)"></block>
-    <result :timer="timer" v-if="!isPlaying"></result>
+    <inputs></inputs>
 </div>
 </template>
 
 
 <script>
-import Block from './components/Block.vue'
-import Result from './components/Result.vue'
+import Inputs from './components/inputs.vue'
+
 export default {
-  components: {Block, Result },
+  components: {Inputs },
   data(){
     return{
-      isPlaying: false,
-      delay: null,
-      timer: null,
-    }
-  },
-
-  methods:{
-    start(){
-      this.isPlaying = true,
-      this.delay = 2000 + Math.random() * 5000
-    },
-
-    emitedFromBlock(timer){
-      this.timer = timer,
-      this.isPlaying = false
+      
     }
   }
 }
---
+++ src/components/inputs.vue
@@ -0,0 +1,40 @@
+<template>
+  <form>
+
+    <input type="email" placeholder="email" v-model="email"><br>
+    <input type="text" placeholder="first name" v-model="firstName"><br>
+    <input type="text" placeholder="second name" v-model="secondName"><br>
+
+    <select v-model="option">
+        <option value="first">first option</option>
+        <option value="second">second option</option>
+    </select><br>
+
+    <input type="checkbox" v-model="checkbox">
+
+    <h3>email: {{email}}</h3><br>
+    <h3>first name: {{firstName}}</h3><br>
+    <h3>second name: {{secondName}}</h3><br>
+    <h3>option: {{option}}</h3><br>
+    <h3>checkBox: {{checkbox}}</h3>
+
+  </form>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email: null,
+            firstName: null,
+            secondName: null,
+            option: null,
+            checkbox: false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
